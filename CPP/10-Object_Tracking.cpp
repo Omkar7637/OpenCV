c53d28ae377b4aa9942ad818e4ab9d3b90b97718
Omkar7637 <omkarkashidok@gmail.com>
2025-12-19 14:15:01 +0530

    Auto update: 2025-12-19 14:15:01

---
+++ CPP/10-Object_Tracking.cpp
@@ -0,0 +1,31 @@
+#include<opencv2/opencv.hpp>
+#include<iostream>
+
+int main()
+{
+
+    // Camera access
+    cv::VideoCapture cap(
+    "nvarguscamerasrc ! video/x-raw(memory:NVMM), width=1920, height=1080, framerate=60/1 ! "
+    "nvvidconv ! video/x-raw, format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink",
+    cv::CAP_GSTREAMER
+    );
+
+    // Camera access cheking
+    if(!cap.isOpened())
+    {
+        std::cout << "Camera Not supported!" << std::endl;
+        return -1;
+    }
+
+    // OpenCV Variables
+    cv::Mat frame, gray, binary, morph;
+    cv::Mat kernal = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(5, 5));
+
+    
+
+    
+    
+
+    return 0;
+}
