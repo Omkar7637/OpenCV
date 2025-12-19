ab629b5632235799461231e0dc58d7360a34a339
Omkar7637 <omkarkashidok@gmail.com>
2025-12-19 12:25:01 +0530

    Auto update: 2025-12-19 12:25:01

---
+++ CPP/09-Contours.cpp
@@ -0,0 +1,43 @@
+#include<opencv2/opencv.hpp>
+#include<iostream>
+
+int main()
+{
+    cv::VideoCapture cap(
+    "nvarguscamerasrc ! video/x-raw(memory:NVMM), width=640, height=480, framerate=60/1 ! "
+    "nvvidconv ! video/x-raw, format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink",
+    cv::CAP_GSTREAMER
+    );
+
+    if(!cap.isOpened())
+    {
+        std::cout << "Camera Not supported!" << std::endl;
+        return -1;
+    }
+
+    cv::namedWindow("Camera", cv::WINDOW_NORMAL);
+    cv::setWindowProperty("Camera", cv::WND_PROP_FULLSCREEN, cv::WINDOW_FULLSCREEN);
+
+    cv::Mat frame, gray, binary, morphopen, morphclose, erode, dilated;
+    std::vector<std::vector<cv::Point>> contours;
+
+    while(true)
+    {
+        cap >> frame;
+        if(frame.empty()) break;
+
+        // Step 1: Convert to gray
+        cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
+
+        // Step 2: Thershold
+        cv::threshold(gray, binary, 0, 255, cv::THRESH_BINARY | cv::THRESH_OTSU)
+
+
+
+        if(cv::waitKey(1) == 27) break;
+    }
+
+    return(0);
+
+
+}
