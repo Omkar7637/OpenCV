48e01ac2f28b4743b733d7d121995a9236f0de40
Omkar7637 <omkarkashidok@gmail.com>
2025-12-30 11:50:01 +0530

    Auto update: 2025-12-30 11:50:01

---
+++ DSA/CPP/002-FindMaximumElementInArray.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+int main()
+{
+    int arr[10] = { 9, 5, 3, 2, 5, 8, 5, 3, 6, 8};
+
+    std::cout << "\n\n";
+
+    std::cout << "Array:";
+    for(int i = 0; i < 10; i++)
+    {
+        std::cout << " " << arr[i];
+    }
+
+    std::cout << "\n";
+
+    int largenumber = 0;
+
+    for(int i = 0; i < 10; i++)
+    {
+        if()
+    }
+
+    std::cout << "Larges elemnet in Array:";
+
+    std::cout << "\n\n";
+
+
+    return(EXIT_SUCCESS);
+}
