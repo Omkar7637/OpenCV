b17bda09a220c5991b4c427de7007021370553dc
Omkar7637 <omkarkashidok@gmail.com>
2026-01-05 11:40:01 +0530

    Auto update: 2026-01-05 11:40:01

--- CPP/11-Clear_Path.cpp
+++ CPP/11-Clear_Path.cpp
@@ -1,13 +1,24 @@
 /*****************************************************************************************
 *   File Name   : Clear_Path.cpp
-*   Discription : Path finding for IGV
+*   Discription : Path finding for IGV using OpenCV and GStreamer
+*                This application captures frames from the Jetson Nano CSI
+*                camera, processes them using OpenCV, and displays the output.
 *
 *   Author      : Omkar Ankush Kashid
 *   Created On  : 29-12-2025
-*   Last Updated: 29-12-2025
+*   Last Updated: 05-01-2026
 *
 *   Platform    : NVIDIA Jetson Nano Super 8GB
 *   OS          : Ubuntu (JetPack)
 *   Language    : C++
 * 
-********************************************************************************************/
+*****************************************************************************************/
+
+// ======================== Header Files ==============================
+#include <opencv2/opencv.hpp> // Realted to OpenCV 
+#include <iostream> // Standered I/O stream Library
+
+int main()
+{
+    
+}
