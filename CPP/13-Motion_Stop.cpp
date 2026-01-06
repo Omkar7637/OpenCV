a55e555a6cf427358f0f4029a871642e46eca555
Omkar7637 <omkarkashidok@gmail.com>
2026-01-06 11:45:01 +0530

    Auto update: 2026-01-06 11:45:01

---
+++ CPP/13-Motion_Stop.cpp
@@ -0,0 +1,11 @@
+/**************************************************************************************
+ *  File Name   : 13-Motion_stop.cpp
+ *  Description : Motion Dectection using fame Differencing
+ *                  Purpose:    
+ *                      - Detect sudden motion in front of the robot
+ *                      - Trigger EMERGENCY STOP if motion exceeds a thershold
+ *                  Technique Used:
+ *                      - Grayscale conversion
+ *                      -     
+ * 
+**************************************************************************************/
