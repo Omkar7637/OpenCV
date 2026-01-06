b216121b6e0c26149051d02afff6ce92430faa86
Omkar7637 <omkarkashidok@gmail.com>
2026-01-06 10:25:01 +0530

    Auto update: 2026-01-06 10:25:01

--- CPP/12-path_decision.cpp
+++ CPP/12-path_decision.cpp
@@ -1,16 +1,19 @@
-/*************************************************************************************
-* File Name:    12-Path_decision.cpp
-* Discription:  Path direction decision (Left/Center/Right) Robot should go Left,
-*               straight, or right.
-*               vision + decision
-*               # Human Thinking vs Robot thinking
-*                   Human: "The path is more open on right, I'll turn right."
-*                   Robot: "There are more WHITE pixel on the right."
-*               So we convert intuition -> Numbers
-*               # Divide the ROI into 3 Zones
-*                   | LEFT | CENTER | RIGHT |
-*                   Each zone:  Count WHITE pixels
-*                               More White = More free space
-* A
+/************************************************************************************************************
+*   File Name   : 12-Path_decision.cpp
+*   Discription : Path direction decision (Left/Center/Right) Robot should go Left, straight, or right.
+*                 vision + decision
+*                 # Human Thinking vs Robot thinking
+*                       Human: "The path is more open on right, I'll turn right."
+*                       Robot: "There are more WHITE pixel on the right."
+*                   So we convert intuition -> Numbers
+*                 # Divide the ROI into 3 Zones
+*                       | LEFT | CENTER | RIGHT |
+*                       Each zone:  Count WHITE pixels
+*                                   More White = More free space
+*   Author      : Omkar Ankush Kashid
+*   Created On  : 06-01-2026
+*   Last updated: 06-01-2026
+*   Platform    : NVIDIA Jetson Nano Super 8GB
+*   OS          : C++   
 
-*************************************************************************************/
+************************************************************************************************************/
