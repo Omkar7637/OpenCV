aff32da1700540b020665cb9527bfb6127a0a72b
Omkar7637 <omkarkashidok@gmail.com>
2026-01-07 15:15:01 +0530

    Auto update: 2026-01-07 15:15:01

--- CPP/15-ROI_to_map.cpp
+++ CPP/15-ROI_to_map.cpp
@@ -36,8 +36,8 @@
 // ========== OCCUPANCY MAP CONFIGRATION ==========
 // Number of rows and columns in the occupancy grid
 // This defines the resolution of the map
-const int MAP_ROWS = 20;
-const int MAP_COLS = 20;
+const int MAP_ROWS = 9*3;
+const int MAP_COLS = 16*3;
 
 // Occupancy map storage
 // 0 = unkown
