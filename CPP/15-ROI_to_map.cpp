543fc54bbfa3c30deabcb5850f7514d31424bf36
Omkar7637 <omkarkashidok@gmail.com>
2026-01-07 14:15:01 +0530

    Auto update: 2026-01-07 14:15:01

---
+++ CPP/15-ROI_to_map.cpp
@@ -0,0 +1,15 @@
+/******************************************************************************************
+ * File Name    : 15-ROI_to_ma.cpp
+ * Project      : IGV Vision System - Path Detection + Emergency Stop
+ * Language     : C++
+ * Library      : OpenCV
+ * 
+ * Desccription : 
+ *  - Capture Live Camera feed
+ *  - Converts image into a binary representation
+ *  - Extaracts bottom-half Region of Interest (ROI)
+ *  - Divides ROI into a grid (occupancy map) 
+ *  - Classifies each grid cell as:
+ *          0 = 
+ * 
+******************************************************************************************/
