dd73dfe871df8ef020c95907bafe636f5acaac91
Omkar7637 <omkarkashidok@gmail.com>
2026-01-07 10:00:01 +0530

    Auto update: 2026-01-07 10:00:01

--- CPP/14-IGV_Preception.cpp
+++ CPP/14-IGV_Preception.cpp
@@ -1,4 +1,16 @@
 /*****************************************************************************************
  *  File Name   : 14-IGV_Preception.cpp
- *  Description : 
+ *  Project     : IGV Vision System - Path Detection + Emergency Stop
+ *  Language    : C++
+ *  Library     : OpenCV
+ * 
+ *  Description :
+ *      - Capture live camera feed
+ *      - Performs preprocessing (Grayscale + blur)
+ *      - Detects sudden motion for EMERGENCY STOP
+ *      - Detects path direction using ROI zoning
+ *      - Displays decision on live video
+ * 
+ *  Designed for : 
+ *      - 
 *****************************************************************************************/
